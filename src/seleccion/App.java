320dfec6de1ef02c692d24e14e709b4ebd52efd0
caro-oviedo <carolina.u.oviedo@gmail.com>
2021-09-27 12:31:16 -0300

    Added Selection sorting algo

---
+++ src/seleccion/App.java
@@ -0,0 +1,15 @@
+package seleccion;
+
+import java.util.Arrays;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Integer[] arreglo = {20,5,69,11,2,105};
+		Seleccion.seleccion(arreglo);
+		System.out.println(Arrays.toString(arreglo));
+		
+	}
+
+}
